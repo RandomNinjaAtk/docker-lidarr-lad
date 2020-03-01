40c6857ba9b458fdef2517e5e5d1526d3b9edd47
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-01 16:16:17 -0500

    Update lidarr-automated-installer-updater.bash

--- root/etc/cont-init.d/lidarr-automated-installer-updater.bash
+++ root/etc/cont-init.d/lidarr-automated-installer-updater.bash
@@ -1,24 +1,109 @@
 #!/bin/bash
 echo "==========start lidarr-automated-installer automated updates==========="
 
-# Check for folder, create folder if needed (hotio docker image compatibility)
-if [ ! -d /config/custom-cont-init.d ]; then
-	mkdir -p /config/custom-cont-init.d
+
+# stop cron
+service cron stop
+
+echo "INSTALLING DEEZLOADER-REMIX"
+
+rm -rf /deezloaderremix && \
+
+if [ -d "/config/xdg" ]; then
+	rm -rf /config/xdg
 fi
 
-if [ -f /config/custom-cont-init.d/lidarr-automated-installer.bash ]; then
-	echo "Previous version detected..."
-	echo "removing....lidarr-automated-installer.bash"
-	rm /config/custom-cont-init.d/lidarr-automated-installer.bash
+if [ ! -d /downloads/deezloaderremix ]; then
+	mkdir /downloads/deezloaderremix
 fi
-if [ ! -f /config/custom-cont-init.d/lidarr-automated-installer.bash ]; then
-	echo "begining updated script installation..."
-	echo "downloading lidarr-automated-installer.bash from: https://github.com/RandomNinjaAtk/lidarr-automated-downloader/blob/master/docker/lidarr-automated-downloader-installer.bash"  && \
-	curl -o "/config/custom-cont-init.d/lidarr-automated-installer.bash" "https://raw.githubusercontent.com/RandomNinjaAtk/lidarr-automated-downloader/master/docker/lidarr-automated-downloader-installer.bash" && \
-	echo "download complete" && \
-	echo "running lsio-automated-installer.bash..." && \
-	bash /config/custom-cont-init.d/lidarr-automated-installer.bash && \
-	rm /config/custom-cont-init.d/lidarr-automated-installer.bash
+
+ln -sf /downloads/deezloaderremix "/root/Deezloader Music" && \
+
+cd / && \
+if [ -f /development.zip  ]; then
+	rm /development.zip 
+	sleep 1s
 fi
+
+wget https://notabug.org/RemixDevs/DeezloaderRemix/archive/development.zip && \
+unzip development.zip && \
+rm development.zip && \
+
+sed -i "s/\"trackNameTemplate\": \"%artist% - %title%\"/\"trackNameTemplate\": \"%disc%%number% - %title% %explicit%\"/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"albumTrackNameTemplate\": \"%number% - %title%\"/\"albumTrackNameTemplate\": \"%disc%%number% - %title% %explicit%\"/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"createAlbumFolder\": true/\"createAlbumFolder\": false/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"embeddedArtworkSize\": 800/\"embeddedArtworkSize\": 1000/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"localArtworkSize\": 1000/\"localArtworkSize\": 1400/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"saveArtwork\": false/\"saveArtwork\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"queueConcurrency\": 3/\"queueConcurrency\": 6/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"maxBitrate\": \"3\"/\"maxBitrate\": \"9\"/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"coverImageTemplate\": \"cover\"/\"coverImageTemplate\": \"folder\"/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"createCDFolder\": true/\"createCDFolder\": false/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"createSingleFolder\": false/\"createSingleFolder\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"removeAlbumVersion\": false/\"removeAlbumVersion\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"syncedlyrics\": false/\"syncedlyrics\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"logErrors\": false/\"logErrors\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"logSearched\": false/\"logSearched\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"trackTotal\": false/\"trackTotal\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"discTotal\": false/\"discTotal\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"explicit\": false/\"explicit\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"barcode\": false/\"barcode\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"unsynchronisedLyrics\": false/\"unsynchronisedLyrics\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"copyright\": false/\"copyright\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"musicpublisher\": false/\"musicpublisher\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"composer\": false/\"composer\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"mixer\": false/\"mixer\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"author\": false/\"author\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"writer\": false/\"writer\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"engineer\": false/\"engineer\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"producer\": false/\"producer\": true/g" "/deezloaderremix/app/default.json" && \
+sed -i "s/\"multitagSeparator\": \"; \"/\"multitagSeparator\": \"andFeat\"/g" "/deezloaderremix/app/default.json" && \
+
+cd /deezloaderremix && \
+npm install && \
+cd /deezloaderremix/app && \
+npm install && \
+cd / && \
+
+if [ ! -d /config/scripts/lidarr-automated-downloader ]; then
+    echo "setting up script lidarr-automated-downloader directory..."
+    mkdir -p /config/scripts/lidarr-automated-downloader
+    echo "done"
+fi
+	
+# Download Scripts
+if [ -f "/config/scripts/lidarr-automated-downloader/lidarr-automated-downloader.bash" ]; then
+	rm /config/scripts/lidarr-automated-downloader/lidarr-automated-downloader.bash
+	sleep 0.1
+fi
+
+if [ ! -f "/config/scripts/lidarr-automated-downloader/lidarr-automated-downloader.bash" ]; then
+    echo "downloading lidarr-automated-downloader.bash from: https://github.com/RandomNinjaAtk/lidarr-automated-downloader/blob/master/lidarr-automated-downloader.bash"
+    curl -o "/config/scripts/lidarr-automated-downloader/lidarr-automated-downloader.bash" "https://raw.githubusercontent.com/RandomNinjaAtk/lidarr-automated-downloader/master/lidarr-automated-downloader.bash"
+    echo "done"
+fi
+
+if [ ! -f "/config/scripts/lidarr-automated-downloader/config" ]; then
+    echo "downloading config from: https://github.com/RandomNinjaAtk/lidarr-automated-downloader/blob/master/config"
+    curl -o "/config/scripts/lidarr-automated-downloader/config" "https://raw.githubusercontent.com/RandomNinjaAtk/lidarr-automated-downloader/master/config"
+    echo "done"
+fi
+
+# Set permissions
+find /config/scripts -type f -exec chmod 0666 {} \;
+find /config/scripts -type d -exec chmod 0777 {} \;
+
+if [ -d "/config/xdg" ]; then
+	chmod 0777 -R /config/xdg
+fi
+
+# Start Deezloader
+echo "Starting Deezloader Remix"
+nohup node /deezloaderremix/app/app.js &>/dev/null &
+sleep 20s
+
+# start cron
+service cron start
+
 echo "==========end start lidarr-automated-installer automated updates==========="
 exit 0
