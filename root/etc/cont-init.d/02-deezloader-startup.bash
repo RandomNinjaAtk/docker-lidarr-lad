760370cc66b501343a80390ef4351ad5ff7e9cf5
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-03-05 18:01:56 -0500

    Bugfix to make sure the modified config is always used

--- root/etc/cont-init.d/02-deezloader-startup.bash
+++ root/etc/cont-init.d/02-deezloader-startup.bash
@@ -1,7 +1,21 @@
 #!/usr/bin/with-contenv bash
 
-if [ ! -d /downloads/deezloaderremix ]; then
-	mkdir /downloads/deezloaderremix
+# remove old config
+if [ -f "/config/xdg/Deezloader Remix/config.json" ]; then
+	rm "/config/xdg/Deezloader Remix/config.json"
+	sleep 0.1
+fi
+
+# clean log folder
+if [ -d "/config/xdg/Deezloader Remix/logs" ]; then
+	rm "/config/xdg/Deezloader Remix/logs"/*
+	sleep 0.1
+fi
+
+# create downloads directory
+if [ ! -d "/downloads/deezloaderremix" ]; then
+	mkdir "/downloads/deezloaderremix"
+	chmod 0777 "/downloads/deezloaderremix"
 fi
 
 ln -sf /downloads/deezloaderremix "/root/Deezloader Music" && \
