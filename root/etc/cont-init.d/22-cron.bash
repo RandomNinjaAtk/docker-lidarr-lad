4a3367a31f0993c2c55995c9283366f2859e59fd
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-04-02 06:58:52 -0400

    Update 22-cron.bash

--- root/etc/cont-init.d/22-cron.bash
+++ root/etc/cont-init.d/22-cron.bash
@@ -1,3 +1,11 @@
 #!/usr/bin/with-contenv bash
 
-service cron start
+# start lad automatically
+if [[ "${ENABLE_LAD}" == "TRUE" ]]; then
+    service cron start
+    echo "Automatic LAD runs eanbled..."
+else
+  echo "Automatic LAD runs disabled..."
+fi
+
+exit $?
