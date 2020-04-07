ddcb6db830d3501af2d9505501eb189ea16e99b7
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-04-07 17:26:37 -0400

    Update 20-dlclient-setup.bash

--- root/etc/cont-init.d/20-dlclient-setup.bash
+++ root/etc/cont-init.d/20-dlclient-setup.bash
@@ -1,24 +1,15 @@
 #!/usr/bin/with-contenv bash
 
 # create downloads directory
-if [ ! -d "/storage/downloads/lidarr/deezloaderremix" ]; then
-	mkdir -p "/storage/downloads/lidarr/deezloaderremix"
-	chmod 0777 "/storage/downloads/lidarr/deezloaderremix"
-fi
-
-if [[ ! -L "/root/Deezloader Music" && ! -d "/root/Deezloader Music" ]]; then
-        ln -sf "/storage/downloads/lidarr/deezloaderremix" "/root/Deezloader Music"
+if [ ! -d "/storage/downloads/lidarr/dlclient" ]; then
+	mkdir -p "/storage/downloads/lidarr/dlclient"
+	chmod 0777 "/storage/downloads/lidarr/dlclient"
 fi
 
 # permissions
 chown -R abc:abc \
 	/xdg/
 	
-chown -R abc:abc \
-	/deezloaderremix/
-
-chmod 0777 -R /deezloaderremix
-
 chown abc:abc \
 	/storage/downloads/lidarr/deezloaderremix
 
