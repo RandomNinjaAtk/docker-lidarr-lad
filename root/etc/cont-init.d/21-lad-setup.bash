9cf86c9dec382d599c7d6640ca3ea26a8a7f58bc
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-04-07 17:37:39 -0400

    Update 21-lad-setup.bash

--- root/etc/cont-init.d/21-lad-setup.bash
+++ root/etc/cont-init.d/21-lad-setup.bash
@@ -49,20 +49,8 @@ fi
 
 # Create config file
 
-if [ -z "$downloadmethod" ]; then
-	downloadmethod="album"
-fi
-if [ -z "$enablefallback" ]; then
-	enablefallback="true"
-fi
 if [ -z "$VerifyTrackCount" ]; then
 	VerifyTrackCount="true"
-fi
-if [ -z "$albumtimeoutpercentage" ]; then
-	albumtimeoutpercentage=8
-fi
-if [ -z "$tracktimeoutpercentage" ]; then
-	tracktimeoutpercentage=25
 fi
 if [ -z "$ReplaygainTagging" ]; then
 	ReplaygainTagging="FALSE"
@@ -81,9 +69,6 @@ if [ -z "$quality" ]; then
 fi
 if [ -z "$ConversionBitrate" ]; then
 	ConversionBitrate="320"
-fi
-if [ -z "$deezloaderurl" ]; then
-	deezloaderurl="http://127.0.0.1:1730"
 fi
 if [ -z "$LidarrUrl" ]; then
 	LidarrUrl="http://127.0.0.1:8686"
@@ -106,31 +91,34 @@ fi
 if [ -z "$RequireQuality" ]; then
 	RequireQuality="false"
 fi
+if [ -z "$CONCURRENCY" ]; then
+	concurrency="4"
+fi
+if [ -z "$ARL_TOKEN" ]; then
+	echo "ARLToken=\"\"" >> "/scripts/lad-config"
+fi
 
 touch "/scripts/lad-config"
 echo 'LidarrApiKey="$(grep "<ApiKey>" /config/config.xml | sed "s/\  <ApiKey>//;s/<\/ApiKey>//")"' >> "/scripts/lad-config"
-echo "downloadmethod=\"$downloadmethod\"" >> "/scripts/lad-config"
-echo "enablefallback=\"$enablefallback\"" >> "/scripts/lad-config"
 echo "VerifyTrackCount=\"$VerifyTrackCount\"" >> "/scripts/lad-config"
-echo "albumtimeoutpercentage=$albumtimeoutpercentage" >> "/scripts/lad-config"
-echo "tracktimeoutpercentage=$tracktimeoutpercentage" >> "/scripts/lad-config"
 echo "ReplaygainTagging=\"$ReplaygainTagging\"" >> "/scripts/lad-config"
 echo "FilePermissions=\"$FilePermissions\"" >> "/scripts/lad-config"
 echo "FolderPermissions=\"$FolderPermissions\"" >> "/scripts/lad-config"
 echo "amount=\"$amount\"" >> "/scripts/lad-config"
 echo "quality=\"$quality\"" >> "/scripts/lad-config"
 echo "ConversionBitrate=\"$ConversionBitrate\"" >> "/scripts/lad-config"
-echo "deezloaderurl=\"$deezloaderurl\"" >> "/scripts/lad-config"
 echo "LidarrUrl=\"$LidarrUrl\"" >> "/scripts/lad-config"
 echo "LidarrImportLocation=\"$LidarrImportLocation\"" >> "/scripts/lad-config"
 echo "downloaddir=\"$downloaddir\"" >> "/scripts/lad-config"
+echo "PathToDLClient=\"$PathToDLClient\"" >> "/scripts/lad-config"
 echo "DownLoadArtistArtwork=\"$DownLoadArtistArtwork\"" >> "/scripts/lad-config"
 echo "BeetConfig=\"/config/scripts/beets-config.yaml\"" >> "/scripts/lad-config"
 echo "BeetLibrary=\"/config/scripts/beets-library.blb\"" >> "/scripts/lad-config"
-echo "BeetLog=\"/config/scripts/beets.log\"" >> "/scripts/lad-config"
 echo "TagWithBeets=\"$TagWithBeets\"" >> "/scripts/lad-config"
 echo "RequireBeetsMatch=\"$RequireBeetsMatch\"" >> "/scripts/lad-config"
 echo "RequireQuality=\"$RequireQuality\"" >> "/scripts/lad-config"
+echo "concurrency=\"$CONCURRENCY\"" >> "/scripts/lad-config"
+echo "ARLToken=\"$ARL_TOKEN\"" >> "/scripts/lad-config"
 
 # Modify script with config location
 sed -i "s/source .\/config/source \/scripts\/lad-config/g" "/config/scripts/lidarr-automated-downloader.bash"
