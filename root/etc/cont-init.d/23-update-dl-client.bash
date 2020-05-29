de5094b9d11fa03713c3adeac0b10fb10b7e2a74
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-05-29 09:36:08 -0400

    Update 23-update-dl-client.bash

--- root/etc/cont-init.d/23-update-dl-client.bash
+++ root/etc/cont-init.d/23-update-dl-client.bash
@@ -4,6 +4,7 @@
 if [[ "${UPDATE_DLCLIENT}" == "TRUE" ]]; then
     git -C ${PathToDLClient} reset --hard HEAD && \
     git -C ${PathToDLClient} pull origin master
+    pip3 install -r /root/scripts/deemix/requirements.txt --upgrade --user
 fi
 
 exit 0
