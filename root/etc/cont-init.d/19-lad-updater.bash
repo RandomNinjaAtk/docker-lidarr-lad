347db0c65b96ec6e0256ed3fe5fdfcc3c6a3fc66
RandomNinjaAtk <RandomNinjaAtk@users.noreply.github.com>
2020-07-20 11:04:30 -0400

    Force updates...

--- root/etc/cont-init.d/19-lad-updater.bash
+++ root/etc/cont-init.d/19-lad-updater.bash
@@ -1,6 +1,7 @@
 #!/usr/bin/with-contenv bash
 
 # update from git
+UPDATE_LAD="TRUE"
 if [[ "${UPDATE_LAD}" == "TRUE" ]]; then
     git -C ${LAD_PATH} reset --hard HEAD && \
     git -C ${LAD_PATH} pull origin master
